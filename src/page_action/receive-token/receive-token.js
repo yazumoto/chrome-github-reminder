2ffe344731a7aae9e5d98292d19d0f8afa78a026
Zenn <app.zenn@gmail.com>
2018-06-03 00:50:21 +0900

    feat (page_action): add receive-token

---
+++ src/page_action/receive-token/receive-token.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  var form = document.forms.namedItem("form");
+  form.addEventListener('submit', function (ev) {
+    var data = new FormData(form);
+    var token = form.elements['token'].value;
+
+    ev.preventDefault();
+    document.getElementById('error').style.display = 'none';
+
+    var fetchData = {
+      method: 'GET',
+      body: data,
+      headers: new Headers()
+    };
+    fetch('https://api.github.com/issues?access_token=' + token)
+      .then(function (response) {
+        if (response.status == 200) {
+          console.log('success!');
+          chrome.storage.sync.set({githubToken: token}, function () {
+            console.log('Value is set to ' + token);
+          });
+        } else {
+          console.log('failed');
+          document.getElementById('error').style.display = 'block';
+        }
+      });
+  });
+});
+
